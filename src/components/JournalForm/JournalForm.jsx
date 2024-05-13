38a2e1aae78a127fdaefd13f45dd7873d0527ea0
moreDasha <moredasha@icloud.com>
2024-05-13 18:49:41 +0400

    feat: add form control by useReducer

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,74 +1,69 @@
 import styles from './JournalForm.module.css';
 import cn from 'classnames';
+import { initialState, formReducer } from './JournalFormState';
 import Button from '../Button/Button';
 import InputName from '../InputName/InputName';
-import { useEffect, useState } from 'react';
-
-const initialValidState = {
-  title: true,
-  date: true,
-  text: true
-};
+import { useEffect, useReducer } from 'react';
 
 function JournalForm ({ onSubmit }) {
 
-  const [validState, setValidState] = useState(initialValidState);
+  const [formState, dispatchForm] = useReducer(formReducer, initialState);
+  const { isValid, values, isReadyToSubmit } = formState;
 
+  // устанавливаем невалидность полей
   useEffect(() => {
     let timerId;
-    if (!validState.title || !validState.date || !validState.text) {
+    if (!isValid.title || !isValid.date || !isValid.text) {
       timerId = setTimeout(() => {
-        setValidState(initialValidState);
+        dispatchForm({ type: 'RESET_VALIDITY'});
       }, 1000);
     }
 
     return () => {
       clearTimeout(timerId);
     };
-  }, [validState]);
+  }, [isValid]);
 
+  // отправка формы
+  useEffect(() => {
+    if (isReadyToSubmit) {
+      onSubmit(values);
+      dispatchForm({ type: 'CLEAR' });
+    }
+  }, [isReadyToSubmit]);
+
+  const onChange = (e) => {
+    dispatchForm({ type: 'SET_VALUE', payload: { [e.target.name]: e.target.value }});
+  };
+
+  // проверяем и сохраняем значения полей формы
   const addJournalItem = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const formProps = Object.fromEntries(formData);
-
-    if (!formProps.title?.trim().length) {
-      setValidState((state) => ({...state, title: false}));
-    } else {
-      setValidState((state) => ({...state, title: true}));
-    }
-    if (!formProps.date) {
-      setValidState((state) => ({...state, date: false}));
-    } else {
-      setValidState((state) => ({...state, date: true}));
-    }
-    if (!formProps.text?.trim().length) {
-      setValidState((state) => ({...state, text: false}));
-    } else {
-      setValidState((state) => ({...state, text: true}));
-    }
-
-    if (formProps.title?.trim().length && formProps.date && formProps.text?.trim().length) {
-      onSubmit(formProps);
-    } else {
-      return;
-    }
+    dispatchForm({ type: 'SUBMIT' });
   };
 
   return (
     <form className={styles['journal-form']} onSubmit={addJournalItem}>
-      <input className={cn(styles['journal-form-input'], styles['input-title'], {[styles['invalid']]: !validState.title})} type="text" name="title" placeholder="Добавьте заголовок"/>
+
+      <input className={cn(styles['journal-form-input'], styles['input-title'], {[styles['invalid']]: !isValid.title})} type="text" name="title" placeholder="Добавьте заголовок" onChange={onChange} value={values.title}/>
+
       <div className={styles['input-wrap']}>
         <div className={styles['input-small-wrap']}>
           <InputName src="/calendar.png" name="Дата"/>
-          <input className={cn(styles['journal-form-input'], styles['input-small'], {[styles['invalid']]: !validState.date})} type="date" name="date"/>
+
+          <input className={cn(styles['journal-form-input'], styles['input-small'], {[styles['invalid']]: !isValid.date})} type="date" name="date" onChange={onChange} value={values.date}/>
+
         </div>
         <div className={styles['input-small-wrap']}>
           <InputName src="/folder.png" name="Теги"/>
-          <input className={cn(styles['journal-form-input'], styles['input-small'])} type="text" name="tag" placeholder="Добавьте теги"/>
+
+          <input className={cn(styles['journal-form-input'], styles['input-small'])} type="text" name="tag" placeholder="Добавьте теги" onChange={onChange} value={values.tag}/>
+
         </div>
       </div>
-      <textarea className={cn(styles['journal-form-input'], {[styles['invalid']]: !validState.text})} name="text" rows="10" placeholder="Добавьте описание"></textarea>
+
+      <textarea className={cn(styles['journal-form-input'], {[styles['invalid']]: !isValid.text})} name="text" rows="10" placeholder="Добавьте описание" onChange={onChange} value={values.text}></textarea>
+
       <Button text="Сохранить"/>
     </form>
   );
